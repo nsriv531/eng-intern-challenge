05bd664c9f8e2ac5983905bdb45692a29dc4b466
nsriv531 <nsriv531@mtroyal.ca>
2024-09-07 13:19:22 -0600

    changed stuff

--- python/translator.py
+++ python/translator.py
@@ -58,8 +58,56 @@ braille_alphabet = {
     'number': '.O.OOO'
 }
 
+modified_alphabet = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O..',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O...OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
+    ' ': '......',
+    ',': '..O...',
+    '.': '.....O',
+    '?': '..O.OO',
+    '!': '..OO.O',
+    "'": '..OOO.',
+    '-': '.O....',
+    '/': '.O.O..',
+    '(': '.O.OO.',
+    ')': '.OO...',
+    ':': '.OO.O.',
+    ';': '.OO.OO',
+    '_': '.OOO..',
+    '"': '.OOO.O',
+    '#': '.OOOO.',
+    '*': '.OOOOO',
+    'capital': '.....O',
+    'number': '.O.OOO'
+}
+
 # Inverse Braille Alphabet Mapping
 inverse_braille_alphabet = {v: k for k, v in braille_alphabet.items()}
+modified_alphabet = {v: k for k, v in braille_alphabet.items()}
 
 def to_braille(text):
     braille_text = ''
@@ -72,10 +120,10 @@ def to_braille(text):
 
         if char.isdigit():
             if not is_number:
-                braille_text += braille_alphabet['number']
+                braille_text += braille_alphabet['number']  # Enter number mode
                 is_number = True
         else:
-            is_number = False
+            is_number = False  # Exit number mode
 
         braille_text += braille_alphabet.get(char, braille_alphabet[' '])  # Default to space if char not found
 
@@ -88,19 +136,18 @@ def to_english(braille_text):
 
     i = 0
     while i < len(braille_text):
-        # Read the current 6-dot Braille character
-        char = braille_text[i:i+6].replace(" ", "")  # Remove extra whitespace characters
-
+    
+        char = braille_text[i:i+6]  # Read the current 6-dot Braille character
         # Check for special markers first (capital or number)
-        if char == braille_alphabet['capital'].replace(" ", ""):
+        if char == braille_alphabet['capital']:
             capitalize_next = True
             i += 6
             continue
-        elif char == braille_alphabet['number'].replace(" ", ""):
+        elif char == braille_alphabet['number']:
             is_number = True
             i += 6
             continue
-        elif char == braille_alphabet[' '].replace(" ", ""):
+        elif char == braille_alphabet[' ']:
             english_text += ' '
             i += 6
             continue
@@ -113,18 +160,23 @@ def to_english(braille_text):
                 continue  # Stay in number mode for digits
             else:
                 is_number = False  # Exit number mode when a non-digit is encountered
-                capitalize_next = False  # Reset capitalize_next flag as well
 
-        # Handle letters
-        elif char in inverse_braille_alphabet:
-            if capitalize_next:
-                english_text += inverse_braille_alphabet[char].upper()
-                capitalize_next = False
+        # Handle letters and capitalization
+        if char in inverse_braille_alphabet:
+            
+            if is_number:
+                if capitalize_next:
+                    english_text += inverse_braille_alphabet[char].upper()
+                    capitalize_next = False
+                else:
+                    english_text += inverse_braille_alphabet[char]
+                
             else:
-                english_text += inverse_braille_alphabet[char]
-
-        else:
-            english_text += '?'  # Placeholder for unrecognized Braille
+                if capitalize_next:
+                    english_text += modified_alphabet[char].upper()
+                    capitalize_next = False
+                else:
+                    english_text += modified_alphabet[char]
 
         i += 6  # Move to the next Braille character
 
@@ -138,7 +190,7 @@ def main():
     text = ' '.join(sys.argv[1:])  # Removed .lower() to preserve original case
 
     # Check if input is already in Braille (starts with capital marker or number marker)
-    if text.startswith('.....') or text.startswith('....O') or text.startswith('....OO'):
+    if text.startswith('.....') or text.startswith('.O.OOO'):
         print(to_english(text))
     else:
         print(to_braille(text))
